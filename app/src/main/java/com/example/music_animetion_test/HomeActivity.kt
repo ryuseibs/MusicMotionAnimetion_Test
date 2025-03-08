9243b800beb849bedba130aefbd48271eaf5f4f2
ryuseibs <beryusei0923212ssyo@aol.com>
2025-03-08 10:12:23 +0900

    Add intent HomeActivity

--- app/src/main/java/com/example/music_animetion_test/HomeActivity.kt
+++ app/src/main/java/com/example/music_animetion_test/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.music_animetion_test
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +19,19 @@ class HomeActivity : AppCompatActivity() {
             insets
         }
 
+        findViewById<Button>(R.id.btnGoToMusicList).setOnClickListener {
+            val intent = Intent(this, MusicListActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.btnGoToAlbumList).setOnClickListener {
+            val intent = Intent(this, AlbumListActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.btnGoToArtistList).setOnClickListener {
+            val intent = Intent(this, ArtistListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
