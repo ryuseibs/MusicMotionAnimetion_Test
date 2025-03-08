78619e895f46316f46e381c2d27babc2214f1db4
ryuseibs <beryusei0923212ssyo@aol.com>
2025-03-08 09:24:23 +0900

    Add MusicListActivity(Get MusicList)

--- app/src/main/java/com/example/music_animetion_test/MusicListActivity.kt
+++ app/src/main/java/com/example/music_animetion_test/MusicListActivity.kt
@@ -1,7 +1,11 @@
 package com.example.music_animetion_test
 
 import android.annotation.SuppressLint
+import android.content.ContentUris
+import android.content.Intent
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +21,29 @@ class MusicListActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val cursor = contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            arrayOf(MediaStore.Audio.Media._ID, MediaStore.Audio.Media.TITLE),
+            null,
+            null,
+            null
+        )
+
+        cursor?.use {
+            val idColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+
+            while (it.moveToNext()) {
+                val id = it.getLong(idColumn)
+                val songUri = ContentUris.withAppendedId(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id
+                )
+                // MusicListActivity 内で曲をタップした時
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("selectedSongUri", songUri.toString())
+                startActivity(intent)
+            }
         }
     }
 }
