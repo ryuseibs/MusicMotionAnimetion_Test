eac9915fcaffb0d639b1a67f494485417a37ea0f
ryuseibs <beryusei0923212ssyo@aol.com>
2025-03-08 10:00:29 +0900

    Add android:id-> Home screen

--- app/src/main/java/com/example/music_animetion_test/HomeActivity.kt
+++ app/src/main/java/com/example/music_animetion_test/HomeActivity.kt
@@ -16,5 +16,6 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
